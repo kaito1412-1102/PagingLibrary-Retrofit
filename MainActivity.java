561bc390bcc4449abee2fdbbb8c7205ef3091892
kaito1412-1102 <68551096+kaito1412-1102@users.noreply.github.com>
2021-06-15 15:43:02 +0700

    display data

--- MainActivity.java
+++ MainActivity.java
@@ -2,7 +2,12 @@ package com.example.paginglibrary;
 
 import android.os.Bundle;
 
+import com.example.paginglibrary.model.MyItem;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.paging.PagedList;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
         rvItem = findViewById(R.id.rv_item);
         rvItem.setLayoutManager(new LinearLayoutManager(this));
         MyItemAdapter adapter = new MyItemAdapter(this);
+        ItemViewModel itemViewModel = ViewModelProviders.of(this).get(ItemViewModel.class);
         rvItem.setAdapter(adapter);
+        itemViewModel.itemPageList.observe(this, new Observer<PagedList<MyItem>>() {
+            @Override
+            public void onChanged(PagedList<MyItem> items) {
+                adapter.submitList(items);
+            }
+        });
     }
 }
